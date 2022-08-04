9bbecd3becd591427bd180ce3c43d66ca16a3d42
rimantaszasytis <rimantaszasytis@gmail.com>
2022-08-04 17:11:58 +0200

    Initial commit

---
+++ js/00-intro.js
@@ -0,0 +1,21 @@
+// singl-line comment
+
+/*
+multi
+line
+comment
+*/
+
+/*
+
+KONCEPTAI:
+
+1) kintamieji - isimena informacija
+2) salygos sakinai - tarpusavio palyginimas
+3) ciklai - darbas su pasikartojancio formato informacija,
+ pasikartojanciu procesu valdymas
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal poreiki
+5) ivykiai - uzsikrove puslapis, paspaude klaveturos mygtuka, paspaude peles 
+mygtuka, pradejo siustis faila, pabaige siustis faila, issiunte uzklausa ir t.t
+6) oop - objektinis programavimas - tai tik "skonio " reikalas
+*/
