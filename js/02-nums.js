9646a8aa9adef9727444a5ef62997c6a4d955ee5
rimantaszasytis <rimantaszasytis@gmail.com>
2022-08-05 11:51:45 +0200

    Numbers

---
+++ js/02-nums.js
@@ -0,0 +1,29 @@
+/* 
+NUMBERS
+teigiami, nulis, neigiami
+teigiami, neigiami
+
+*/
+
+
+const teigiamas = 5;
+const neigiamas = -8;
+const nulis = 0;
+const nulisMinus = -0;
+
+console.log(nulis, nulisMinus);
+
+const desimtainis = 3.141592554;
+console.log(desimtainis);
+
+const begalybe = Infinity;
+const begalybeMinus = -Infinity;
+console.log(begalybe, begalybeMinus);
+
+
+const nesamone = NaN;
+console.log(nesamone);
+
+const puseZodzio = 'labas' / 2;
+console.log(puseZodzio);
+
